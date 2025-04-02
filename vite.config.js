15a765a86d4eb66179ba86493f640541a77d2e89
execoffex <execoffex@github.com>
2025-04-03 00:42:34 +0300

    fix-base

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Add this base property:
-  base: '/executive-coffee-landing/', // Make sure this matches your repo name!
+  base: '/', // Make sure this matches your repo name!
 })
